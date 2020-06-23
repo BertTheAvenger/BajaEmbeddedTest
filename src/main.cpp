297acd0eaf62757126018c29368bd40a1ac4d125
BertTheAvenger <>
2020-06-23 13:01:15 -0700

    changes...?

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #include <ModuleCore.h>
 #include "wifi_secrets.h"
 
+#define DEAD_ZONE 5
+#define SET_POINT 512
+
+#define DEAD_MIN SET_POINT-DEAD_ZONE
+#define DEAD_MAX SET_POINT+DEAD_ZONE
+
 #define EN12 D1
 #define EN34 D7
 #define PWM12 D3
@@ -23,21 +29,35 @@ void statusRequestListener(DynamicJsonDocument inDoc) {
     core.sendDoc("status", doc);
 }
 
+void setMotor(uint8_t pinEN, uint8_t pinPWM, uint16_t pwm) {
+    if(pwm > DEAD_MIN && pwm < DEAD_MAX) {
+        digitalWrite(pinEN, LOW);
+        analogWrite(pinPWM, SET_POINT);
+    } else {
+        digitalWrite(pinEN, HIGH);
+        analogWrite(pinPWM, pwm);
+    }
+
+}
+
 void setMotorSpeedsListener(DynamicJsonDocument inDoc){
     Serial.println("SetMotorSpeeds");
+    setMotor(EN12, PWM12, inDoc["m1"].as<uint16_t>());
+    setMotor(EN34, PWM34, inDoc["m2"].as<uint16_t>());
 }
 
 void setM1SpeedListener(DynamicJsonDocument inDoc){
-    Serial.println("SetM1Speed");
-    Serial.println(inDoc["speed"].as<uint16_t>());
-    analogWrite(PWM12, inDoc["speed"].as<uint16_t>());
-    digitalWrite(EN12, HIGH);
+    //Serial.println("SetM1Speed");
+    //Serial.println(inDoc["speed"].as<uint16_t>());
+    //inDoc["speed"].as<uint16_t>()
+    setMotor(EN12, PWM12, inDoc["speed"].as<uint16_t>());
+
+
 }
 
 void setM2SpeedListener(DynamicJsonDocument inDoc){
-    Serial.println("SetM2Speed");
-    analogWrite(PWM34, inDoc["speed"].as<uint16_t>());
-    digitalWrite(EN34, HIGH);
+    //Serial.println("SetM2Speed");
+    setMotor(EN34, PWM34, inDoc["speed"].as<uint16_t>());
 }
 
 void setup() {
